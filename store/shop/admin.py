4b7c7d56c98d5f0a7d67914f26be1efda3e305ee
anamadame <150360126+anamadame@users.noreply.github.com>
2024-03-05 15:09:48 +0600

    Update admin.py

--- store/shop/admin.py
+++ store/shop/admin.py
@@ -11,16 +11,4 @@ admin.site.register(Basket)
 admin.site.register(Favorite)
 admin.site.register(User)
 
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Brand)
-admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(ProductPhotos)
-admin.site.register(Review)
-admin.site.register(Rating)
-admin.site.register(Basket)
-admin.site.register(Favorite)
-admin.site.register(User)
 
